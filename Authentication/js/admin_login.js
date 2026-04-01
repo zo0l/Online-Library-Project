fc815e958bc050b654dda2c596212fc01633bae8
zo0l <abdallah.mohammed.cyber@gmail.com>
2026-04-01 04:32:53 +0300

    add js fiels

---
+++ Authentication/js/admin_login.js
@@ -0,0 +1,23 @@
+//admin login
+
+document.querySelector(".login-form").onsubmit = function(e) {
+
+    e.preventDefault();
+
+    let username = document.querySelector("input[name='username']").value;
+    let password = document.querySelector("input[name='password']").value;
+
+
+     if (username.length < 3 ) {
+        alert("No user with this username");
+    }
+    else if (password.length < 6 ) {
+        alert("Password is incorrect");
+    }
+    else {
+         window.location.href = "../admin pages/admin-dashboard.html";
+        alert("Login successful");
+
+    }
+
+};
